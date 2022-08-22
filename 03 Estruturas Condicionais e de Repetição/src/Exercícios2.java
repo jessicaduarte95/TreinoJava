9770f5fca0d4d5f22ad3f8f0f7de59bdf76be026
jessicaduarte95 <jessicaduarte.95@gmail.com>
2022-08-22 20:11:39 -0300

    Exercícios - Estruturas Condicionais

--- 03 Estruturas Condicionais e de Repetição/src/Exercícios2.java
+++ 03 Estruturas Condicionais e de Repetição/src/Exercícios2.java
@@ -60,14 +60,20 @@ public class Exercícios2 {
         /*Leia a idade de 20 pessoas e exiba a média das idades.*/
 
         int sumIdade = 0;
-        for (int i = 0; i < idade.length; i++) {
+        for (int i = 0; i < idade.length; i++){
             sumIdade += idade[i];
         }
         System.out.println("Média das Idades: " + sumIdade/idade.length);
 
         /*Leia a idade de 20 pessoas e exiba quantas pessoas são maiores de idade.*/
 
-
+        int somaMaiorIdade = 1;
+        for(int i = 0; i < idade.length; i++){
+            if(idade[i] >= 18){
+                somaMaiorIdade = somaMaiorIdade + 1;
+            }
+        }
+        System.out.println("Quantidade de pessoas maiores de idade: " + somaMaiorIdade);
 
         /*Leia o nome e a idade de 10 pessoas e exiba o nome da pessoa mais nova.*/
 
@@ -81,21 +87,56 @@ public class Exercícios2 {
         /*Escreva um algoritmo que leia 20 números do usuário e exiba quantos números são
         maiores do que 8.*/
 
-
+        int[] numeros = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int maiorNum = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if(numeros[i] > 8){
+                maiorNum += 1;
+            }
+        }
+        System.out.println("Quantidade de números maiores que oito: " + maiorNum);
 
         /*Escreva um algoritmo que leia 20 números do usuário e exiba quantos números são
         Pares.*/
 
+        int numPar = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if(numeros[i]%2 == 0){
+                numPar += 1;
+            }
+        }
+        System.out.println("Quantidade de números pares: " + numPar);
 
         /*Faça um algoritmo que leia 20 números e, ao final, escreva quantos estão entre 0 e
         100.*/
 
-
+        int quant = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if(numeros[i] < 100 & numeros[i] > 0){
+                quant += 1;
+            }
+        }
+        System.out.println("Quantidade de números entre 0 e cem: " + quant);
 
         /*Faça um algoritmo que leia 20 números e, ao final, escreva quantos estão entre 0 e
         100, quantos estão entre 101 e 200 e quantos são maiores de 200.*/
 
-
+        int[] numerosNovo = {10,201,3,4,5,160,7,800,9,10,110,120,13,14,15,16,17,18,19,20};
+        int ateCem = 0;
+        int maiorQueCem = 0;
+        int maiorQueDuzentos = 0;
+        for (int i = 0; i < numerosNovo.length; i++) {
+            if(numerosNovo[i] > 0 & numerosNovo[i] < 100){
+                ateCem += 1;
+            }else if(numerosNovo[i] > 100 & numerosNovo[i] < 200){
+                maiorQueCem += 1;
+            }else if(numerosNovo[i] > 200){
+                maiorQueDuzentos += 1;
+            }
+        }
+        System.out.println("Quantidade entre 0 e 100: " + ateCem);
+        System.out.println("Quantidade entre 0 e 100: " + maiorQueCem);
+        System.out.println("Quantidade entre 0 e 100: " + maiorQueDuzentos);
 
         /*Escreva um algoritmo que leia uma sequência de números do usuário e realize a
         soma desses números. Encerre a execução quando um número negativo for digitado.*/
