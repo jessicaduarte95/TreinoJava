4c4692dca3a1fd91ad1d7b12b7ea51db68d14cc4
jessicaduarte95 <jessicaduarte.95@gmail.com>
2022-08-26 16:41:53 -0300

    Exercícios Arrays

--- 04 Arrays/src/Exercícios.java
+++ 04 Arrays/src/Exercícios.java
@@ -6,19 +6,45 @@ public class Exercícios {
         vai receber os mesmo valores do primeiro utilizando atribuição. B[0] = A[0]. Deve-se
         usar um laço de Repetição e não atribuir de forma direta os valores.*/
 
+        int[] arrayA = {1,2,3,4,5};
 
+        int[] arrayB = new int[5];
+
+        for (int i = 0; i < arrayA.length; i++){
+            arrayB[i] = arrayA[i];
+            System.out.println(arrayB[i]);
+        }
+
+        System.out.println("[" + arrayB[0] + ", " + arrayB[1] + ", " + arrayB[2] + ", " + arrayB[3] + ", " + arrayB[4] + "] ");
+        System.out.println("------------------");
 
         /*Criar um array de 10 posições de tipo inteiro que vai receber 10 valores. Criar também
         um segundo array de tipo String que vai receber o valor do primeiro multiplicado por 5
         com a seguinte descrição: "O valor multiplicado por 5 é:"*/
 
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int[] arrayResult = new int[10];
 
+        for(int i = 0; i < array.length; i++){
+            arrayResult[i] = array[i] * 5;
+            System.out.println(arrayResult[i]);
+        }
+        System.out.println("--------------------------");
 
         /*Criar dois arrays de tipo inteiro de tamanho 10 e inicializar os dois com valores
-        diferentes. Criar também um segundo array de 10 posições que vai receber como valor
+        diferentes. Criar também um  array de 10 posições que vai receber como valor
         para cada posição a subtração do array A pelo array B.
         C = A - B */
 
+        int[] A = {3,10,9,18,16,18,29,26,28,50}; 
+        int[] B = {3,6,9,12,15,18,21,24,27,30};
+
+        int[] resultadoDeC = new int[10];
+
+        for(int i = 0; i < A.length; i++){
+            resultadoDeC[i] = A[i] - B[i];
+            System.out.println(resultadoDeC[i]);
+        }
 
 
         /*Criar e inicializar um array de inteiros de tamanho 10. Criar também um fluxo que vai ler
